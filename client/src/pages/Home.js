67f1a4d2089656144e143a353e7c4d4800879357
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-08-29 18:01:01 -0400

    Add home, login, signup, and header, footer

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+
+  return (
+    <main>
+      
+      <div className="flex-row justify-center">
+        Home, type your SWOT
+      </div>
+    </main>
+  );
+};
+
+export default Home;

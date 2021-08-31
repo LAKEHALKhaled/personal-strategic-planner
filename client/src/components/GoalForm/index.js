05503cd63f7153028236bf50d00d67d2fb19e971
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-08-30 20:45:35 -0400

    added GoalForm and GoalList

--- client/src/components/GoalForm/index.js
+++ client/src/components/GoalForm/index.js
@@ -1,1 +1,88 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
 
+import { ADD_GOAL } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
+
+const GoalForm = ({ areaId }) => {
+  const [goalText, setGoalText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addGoal, { error }] = useMutation(ADD_GOAL);
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addGoal({
+        variables: {
+          areaId,
+          goalText,
+          goalAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setGoalText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'goalText' && value.length <= 280) {
+      setGoalText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div>
+      <h4> what are the goals you want to acheave in this area?</h4>
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Character Count: {characterCount}/280
+            {error && <span className="ml-2">{error.message}</span>}
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="goalText"
+                placeholder="Add your goal..."
+                value={goalText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+                Add a goal
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to add new goals. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default GoalForm;
--- client/src/components/GoalList/index.js
+++ client/src/components/GoalList/index.js
@@ -1,1 +1,37 @@
 
+import React from 'react';
+
+const GoalList = ({ goals = [] }) => {
+  if (!goals.length) {
+    return <h3>No Goals are set, please enter your goals that you want to acheave!</h3>;
+  }
+
+  return (
+    <>
+      <h3
+        className="p-5 display-inline-block"
+        style={{ borderBottom: '1px dotted #1a1a1a' }}
+      >
+        Goals
+      </h3>
+      <div className="flex-row my-4">
+        {goals &&
+          goals.map((goal) => (
+            <div key={goal._id} className="col-12 mb-3 pb-3">
+              <div className="p-3 bg-dark text-light">
+                <h5 className="card-header">
+                  {goal.goalAuthor} goal added {' '}
+                  <span style={{ fontSize: '0.825rem' }}>
+                    on {goal.createdAt}
+                  </span>
+                </h5>
+                <p className="card-body">{goal.goalText}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default GoalList;

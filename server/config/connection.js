a2f597184118e856a8fe40683e278f2d60e18906
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-09-06 20:24:24 -0400

    changed db name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/life-goals',
+  process.env.MONGODB_URI || 'mongodb://localhost/PSP',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
